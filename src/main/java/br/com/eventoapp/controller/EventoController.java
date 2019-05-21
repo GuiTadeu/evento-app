ce3f4bc7bc7469c9ebe653ebc7e7c017f92d42eb
GuiTadeu <guitadeu.py@gmail.com>
2019-05-21 19:15:38 -0300

    [ADD] CRUD

--- src/main/java/br/com/eventoapp/controller/EventoController.java
+++ src/main/java/br/com/eventoapp/controller/EventoController.java
@@ -1,11 +1,15 @@
 package br.com.eventoapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.eventoapp.model.Convidado;
 import br.com.eventoapp.model.Evento;
@@ -27,8 +31,13 @@ public class EventoController {
 	}
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
-	public String form(Evento evento) {
+	public String form(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Tá errado mermão, preenche isso aê!");
+			return "redirect:/cadastrarEvento";
+		}
 		er.save(evento);
+		attributes.addFlashAttribute("mensagem", "Que lindo! Evento cadastrado com sucesso!");
 		return "redirect:/cadastrarEvento";
 	}
 	
@@ -52,11 +61,41 @@ public class EventoController {
 		return mv;
 	}
 	
+	@RequestMapping("/deletar")
+	public String deletarEvento(long codigo) {
+		
+		Evento evento = er.findByCodigo(codigo);
+		
+		// Cascade
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		cr.deleteAll(convidados);
+		
+		er.delete(evento);
+		return "redirect:/";
+	}
+	
 	@RequestMapping(value="/{codigo}", method=RequestMethod.POST)
-	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado ) {
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result, RedirectAttributes attributes ) {
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Tá errado mermão, preenche isso aê!");
+			return "redirect:/{codigo}";
+		}
+		
 		Evento evento = er.findByCodigo(codigo);
 		convidado.setEvento(evento);
 		cr.save(convidado);
+		attributes.addFlashAttribute("mensagem", "Que lindo! Convidado cadastrado com sucesso!");
 		return "redirect:/{codigo}";
+	}
+	
+	@RequestMapping("/deletarConvidado")
+	public String deletarConvidado(String rg) {
+		Convidado convidado = cr.findByRg(rg);
+		cr.delete(convidado);
+		
+		Evento evento = convidado.getEvento();
+		long codigo = evento.getCodigo();
+		
+		return "redirect:/" + codigo;
 	}
 }
